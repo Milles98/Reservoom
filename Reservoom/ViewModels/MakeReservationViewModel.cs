fe8ff284afb16fe4414a51cca89a2167b9ac05ad
Mille Elfver <mille.elfver98@gmail.com>
2024-06-11 15:14:38 +0200

    Major update, added many viewmodels connected

---
+++ Reservoom/ViewModels/MakeReservationViewModel.cs
@@ -0,0 +1,74 @@
+﻿using System.Windows.Input;
+
+namespace Reservoom.ViewModels;
+
+public class MakeReservationViewModel : ViewModelBase
+{
+    private string _userName;
+
+    public string UserName
+    {
+        get => _userName;
+        set
+        {
+            _userName = value;
+            OnPropertyChanged(nameof(UserName));
+        }
+    }
+
+    private int _roomNumber;
+
+    public int RoomNumber
+    {
+        get => _roomNumber;
+        set
+        {
+            _roomNumber = value;
+            OnPropertyChanged(nameof(RoomNumber));
+        }
+    }
+
+    private int _floorNumber;
+
+    public int FloorNumber
+    {
+        get => _floorNumber;
+        set
+        {
+            _floorNumber = value;
+            OnPropertyChanged(nameof(FloorNumber));
+        }
+    }
+
+    private DateTime _startDate;
+
+    public DateTime StartDate
+    {
+        get => _startDate;
+        set
+        {
+            _startDate = value;
+            OnPropertyChanged(nameof(StartDate));
+        }
+    }
+
+    private DateTime _endDate;
+
+    public DateTime EndDate
+    {
+        get => _endDate;
+        set
+        {
+            _endDate = value;
+            OnPropertyChanged(nameof(EndDate));
+        }
+    }
+
+    public ICommand SubmitCommand { get; }
+    public ICommand CancelCommand { get; }
+
+    public MakeReservationViewModel()
+    {
+        
+    }
+}
---
+++ Reservoom/ViewModels/ReservationViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Reservoom.Models;
+
+namespace Reservoom.ViewModels;
+
+public class ReservationViewModel : ViewModelBase
+{
+    private readonly Reservation _reservation;
+    
+    public string RoomId => _reservation.RoomId.ToString();
+    public string Username => _reservation.Username;
+    public string StartDate => _reservation.StartTime.ToString("d");
+    public string EndDate => _reservation.EndTime.ToString("d");
+
+    public ReservationViewModel(Reservation reservation)
+    {
+        _reservation = reservation;
+    }
+}
