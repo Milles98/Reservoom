5c9121de2b16eb9eb5d33d1933cc72ed637f9b39
Mille Elfver <mille.elfver98@gmail.com>
2024-06-11 13:17:00 +0200

    Added all new models with logic

---
+++ Reservoom/Exceptions/ReservationConflictException.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.Serialization;
+using Reservoom.Models;
+
+namespace Reservoom.Exceptions;
+
+public class ReservationConflictException : Exception
+{
+    public Reservation ExistingReservation { get; }
+    public Reservation IncomingReservation { get; }
+    
+    public ReservationConflictException(Reservation incomingReservation, Reservation existingReservation)
+    {
+        IncomingReservation = incomingReservation;
+        ExistingReservation = existingReservation;
+    }
+
+    public ReservationConflictException(string? message, Reservation incomingReservation, Reservation existingReservation) : base(message)
+    {
+        IncomingReservation = incomingReservation;
+        ExistingReservation = existingReservation;
+    }
+
+    public ReservationConflictException(string? message, Exception? innerException, Reservation incomingReservation, Reservation existingReservation) : base(message, innerException)
+    {
+        IncomingReservation = incomingReservation;
+        ExistingReservation = existingReservation;
+    }
+}
